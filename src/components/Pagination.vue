112430369c864058dcdc9f89c36753e7c48c7411
Lindsay <lindsayeggers@gmail.com>
2020-04-09 12:03:11 -0400

    add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="columns is-centered has-text-centered">
+		<nav class="is-centered" role="navigation" aria-label="pagination">
+			<button class="button is-light is-size-6" @click="prevPage" :disabled="isDisabled">
+				Previous
+			</button>
+
+			<button class="button is-light is-size-6" @click="nextPage">
+				Next page
+			</button>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	Name: 'Pagination',
+	props: {
+		page: {
+			type: Number,
+			required: true
+		}
+	},
+	computed: {
+		isDisabled() {
+			return this.page == 1;
+		}
+	},
+	methods: {
+		nextPage() {
+			this.$emit('nextPage');
+		},
+		prevPage() {
+			this.$emit('prevPage');
+		}
+	}
+}
+</script>
--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -4,17 +4,25 @@
 
 			<Tile :users="users" />
 
+      <Pagination
+				:page="page"
+				@nextPage="getNextPage"
+				@prevPage="getPrevPage"
+			/>
+
 		</div>
 	</section>
 </template>
 
 <script>
 import axios from 'axios';
+import Pagination from '@/components/Pagination.vue'
 import Tile from '@/components/Tile.vue'
 
 export default {
   name: "Results",
   components: {
+    Pagination,
     Tile
   },
   data() {
@@ -24,6 +32,16 @@ export default {
 		}
 	},
   methods: {
+    async getNextPage() {
+			this.page += 1;
+			if (this.page == 0) this.page = 1;
+			await this.getUsers();
+		},
+		async getPrevPage() {
+			this.page -= 1;
+			if (this.page == 0) this.page = 1;
+			await this.getUsers();
+		},
     async getUsers() {
 			const { data: { results } } = await axios.get(`https://randomuser.me/api/?page=${this.page}&results=10&seed=random`);
 			this.users = results;
