ea7b931d28b6c2cebdf843f992a5302016cabcc3
Lindsay <lindsayeggers@gmail.com>
2020-04-09 11:40:21 -0400

    initial setup removing default vue-cli messaging

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<header class="header">
+			<nav class="container navbar">
+			</nav>
+		</header>
+
+		<main class="main">
+			<router-view></router-view>
+		</main>
+
+		<footer class="footer">
+			<div class="content has-text-centered">
+				<p>&copy; Lindsay Eggers</p>
+			</div>
+		</footer>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
+button,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Quicksand', sans-serif;
+}
+#app {
+	display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+.header {
+	border-bottom: 1px solid #eee;
+}
+.main {
+	flex: 1;
+}
+.footer {
+	background-color: #eee;
+}
+.button {
+	font-weight: 700;
+}
+.capitalize {
+	text-transform: capitalize;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import router from './router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: h => h(App),
+}).$mount('#app');
