68ea55656177172359040c6442f31a2f145d054d
Lindsay <lindsayeggers@gmail.com>
2020-04-09 11:58:32 -0400

    connect API call to grab real user data

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -1,13 +1,18 @@
 <template>
 	<div class="columns is-centered">
 		<div class="column tile is-parent is-vertical is-three-fifths">
-			<div class="columns tile is-child box has-text-centered-touch">
+			<div class="columns tile is-child box has-text-centered-touch" v-for="user in users" :key="user.id.value">
+				<div class="column is-narrow">
+					<img :alt="`${user.name.first} ${user.name.last} Image`"
+					:src="user.picture.large"/>
+				</div>
+
 				<article class="column">
-					<p class="capitalize"><strong>FirstName LastName</strong></p>
-					<p class="capitalize">Age: Age</p>
-					<p class="capitalize">Gender: Gender</p>
-					<p>Email: email</p>
-					<p>Phone: phone</p>
+					<p class="capitalize"><strong>{{ user.name.first }} {{ user.name.last }}</strong></p>
+					<p class="capitalize">Age: {{ user.dob.age }}</p>
+					<p class="capitalize">Gender: {{ user.gender }}</p>
+					<p>Email: {{ user.email }}</p>
+					<p>Phone: {{ user.phone }}</p>
 				</article>
 			</div>
 		</div>
@@ -16,6 +21,12 @@
 
 <script>
 export default {
-	name: 'Tile'
+	name: 'Tile',
+	props: {
+		users: {
+			type: Array,
+			required: true
+		}
+	}
 }
 </script>
--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -2,19 +2,35 @@
   <section class="section">
 		<div class="container">
 
-			<Tile />
+			<Tile :users="users" />
 
 		</div>
 	</section>
 </template>
 
 <script>
-import Tile from '@/components/tile.vue'
+import axios from 'axios';
+import Tile from '@/components/Tile.vue'
 
 export default {
   name: "Results",
   components: {
     Tile
+  },
+  data() {
+		return {
+			page: 1,
+			users: []
+		}
+	},
+  methods: {
+    async getUsers() {
+			const { data: { results } } = await axios.get(`https://randomuser.me/api/?page=${this.page}&results=10&seed=random`);
+			this.users = results;
+		}
+  },
+  async created() {
+		await this.getUsers();
   }
 }
 </script>
