24bc2b4628d69dfbaa12c176af18426ffea4e3f0
Lindsay <lindsayeggers@gmail.com>
2020-04-09 11:44:16 -0400

    add Home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="section">
+		<div class="container">
+			<div class="rows has-text-centered">
+				<article class="introduction row is-full">
+					<p><strong>Welcome to My Directory</strong>, a directory of users.</p>
+				</article>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Home'
+}
+</script>
+
+<style scoped>
+.introduction {
+	padding-bottom: 3rem;
+}
+</style>
